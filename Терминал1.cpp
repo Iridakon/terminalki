65752d2abe2e631978fb1ec8ebaf9f1b252704f0
Iridakon <71597136+Iridakon@users.noreply.github.com>
2020-09-21 20:41:42 +0700

    Update Терминал1.cpp

--- Терминал1.cpp
+++ Терминал1.cpp
@@ -1,4 +1,4 @@
-﻿#include <stdio.h>
+#include <stdio.h>
 #include <math.h>
 #include <locale.h>
 #define p 3.1415
@@ -6,6 +6,7 @@
 int main()
 {
 	setlocale(LC_ALL, "Rus");
+	printf("Введите числа a, b,n, где a, b - коэфиценты в записи a+i*b, а n- степень \n");
 	double r = 0, a = 0, b = 0, n = 0, f=0;
 	scanf_s("%lf %lf %lf", &a, &b,&n);
 	r = sqrt(a * a + b * b);
@@ -13,19 +14,20 @@ int main()
 	s = b / r;
 	f = asin(s);
 	c = f / p;
-	printf("0. z= %f (cos %f p + i sin %f p)\n", r, c, c);
+	printf("0. z= %f (cos %f p + i*sin %f p)\n", r, c, c);
 	double a1 = 0, b1 = 0;
 	a1 = a / r;
 	b1 = -b / r;
-	printf("1. z^(-1)= %lf +( %lf)*i )\n", a1,b1);
+	printf("1. z^(-1)= %lf +( %lf)*i \n", a1,b1);
 	double r1 = pow(r, n);
 	printf("2. z^n= %lf (cos %f p + i sin %f p)\n", r1, c*n, c * n);
 	double r2 = pow(r, -n);
 	double del = 2 * p/n;
+	printf("2. z^-n= \n");
 	for (int i = 0; i < n; i++) {
-		c = c / n;
+		c = (c+2*p*i)/n;
 
-		printf("2. z^-n= %lf (cos %f p + i sin %f p)\n", r2, c+del*i , c +del * i);
+		printf("2. z^-n= %lf (cos %f p + i sin %f p)\n", r2, c,c);
 	}
-
+	return 0;
 }
